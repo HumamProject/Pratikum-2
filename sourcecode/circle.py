0d5eb31de889f1180fbb0f54223d8fc120c81511
HumamProject <fdilumam@gmail.com>
2021-11-04 21:02:09 +0700

    menghitung luas dan keliling lingkaran dengan python

---
+++ sourcecode/circle.py
@@ -0,0 +1,17 @@
+#nama saya
+print ( "program menghitung luas lingkaran dan keliling lingkarang \n " )
+print ( "nama muhammad fadhil humam \ nNIm:312110394" )
+print ( "tanggal pengerjaan tugas 31 oktober 2021 \n " )
+
+#menghitung luas lingkaran
+phi  =  3,14
+r  =  float ( input ( " masukan panjang jari jari lingkaran : " ))
+luas_lingkaran  =  phi * r * r
+
+print ( "luas lingkaran adalah :" , luas_lingkaran )
+
+#menghitung keliling lingkaran
+r  =  float ( input ( " masukan jari jari lingkaran :" ))
+keliling_lingkaran  =  phi * r
+
+print ( "keliling lingkaran adalah :" , keliling_lingkaran )
